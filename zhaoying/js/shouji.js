a1b96e9155130b7a97796cb7e696a9d8c9469545
赵颖 <1130051785@qq.com>
2019-11-28 17:13:50 +0800

    2019

--- zhaoying/js/shouji.js
+++ zhaoying/js/shouji.js
@@ -1,6 +1,12 @@
 var Search = document.getElementById('search');
 Search.onclick=function(){
-	alert('2333');
-	Search.style.placeholder=='请输入搜索的商品';
+	//alert('2333');
+	Search.placeholder=='请输入搜索的商品';
 	
 }
+// Search.onclick = function(){
+// 	Search.placeholder.innerHTML = '请输入搜索的商品'; 
+// }
+// var val = Search.placeholder;//获取属性;
+// 		Search.placeholder = val; //设置属性;
+// 		console.log(val);
