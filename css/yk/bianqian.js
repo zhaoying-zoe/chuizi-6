fd121ddf96d51839786638737cfe8c2b439885d0
杨凯 <372492384@qq.com>
2019-12-30 21:07:28 +0800

    12/30

---
+++ css/yk/bianqian.js
@@ -0,0 +1,14 @@
+var aBtn = document.querySelectorAll('.main .nav li a');
+// console.log(aBtn);
+
+for(var i =0;i<aBtn.length;i++){
+	// console.log(i);
+	aBtn[i].index=i;
+		aBtn[i].onclick=function(){
+		for(var l = 0;l<aBtn.length;l++){
+			aBtn[l].className='nav-item-a';
+		}
+		this.className='active nav-item-a'
+	}
+		
+}
