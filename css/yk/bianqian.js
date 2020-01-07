e3de1a47b1e2b49ef2b4eb2e453106eceba88444
杨凯 <372492384@qq.com>
2020-01-07 13:18:53 +0800

    1/7

--- css/yk/bianqian.js
+++ css/yk/bianqian.js
@@ -14,11 +14,48 @@ for(var i =0;i<aBtn.length;i++){
 }
 
 //购物车鼠标移入
-var Ocart = document.querySelector('.cart')
+var oCart = document.querySelector('.cart')
 var oCartHide = document.querySelector('.cart-hide')
-Ocart.onmouseenter = function(){
-	oCartHide.style.display = 'block'
+var oCartTime = 0
+
+function oCartDs(){
+	oCartTime = setTimeout(function(){
+			oCart.onmouseenter = function(){
+			oCartHide.style.display = 'block'
+		}
+		oCart.onmouseleave = function(){
+			oCartHide.style.display = 'none'
+		}
+
+	},1000)
 }
-Ocart.onmouseleave = function(){
-	oCartHide.style.display = 'none'
+oCart.onmouseenter = function(){
+	clearTimeout(oCartTime)
+}
+oCart.onmouseleave = function(){
+	oCartDs()
+}
+
+
+//切换多种语言鼠标移入
+var oLanguage = document.querySelector('.language')
+var oLanguageYc = document.querySelector('.language-yc')
+var oLanguageTime = 0
+clearTimeout(oLanguageTime)
+function oLanguageDs(){
+		oLanguageTime = setTimeout(function(){
+			oLanguage.onmouseenter = function(){
+				oLanguageYc.style.display = 'block'
+		}
+			oLanguage.onmouseleave = function(){
+				oLanguageYc.style.display = 'none'
+		}
+	},500)
+}
+
+oLanguage.onmouseenter = function(){
+	clearTimeout(oLanguageTime)
+}
+oLanguage.onmouseleave = function(){
+	oLanguageDs()
 }
